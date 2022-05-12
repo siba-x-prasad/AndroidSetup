8bf891269bbc3be4d000e4e99c2e8e803626b795
siba-x-prasad <siba.x.prasad@gmail.com>
2022-05-12 19:31:25 +0100

    Android Project Set Up

---
+++ app/build/generated/source/buildConfig/goldFlavors/debug/com/swasi/setup/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.swasi.setup;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.swasi.gold";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "goldFlavors";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0-debug";
+}
